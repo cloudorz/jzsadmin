60e1d39f1972ea06ee9498b8944f3bf2e2d6113a
cloud <cloudcry@gmail.com>
2012-04-03 15:33:26 +0800

    smoe chagnes

--- jzsadmin/static/admin/js/menu.js
+++ jzsadmin/static/admin/js/menu.js
@@ -8,7 +8,7 @@ a.config.closeSameLevel=false;
 a.add(0,-1,'<B>家政发布管理</B>','javascript: void(0);');
 
 a.add(1, 0,'发布管理','');
-a.add(2, 1,'审核列表','/admin/entry/wait/');
+a.add(2, 1,'审核列表','/admin/entry/status/');
 
 a.add(10, 0,'内容管理','');
 a.add(11, 10,'信息条目','/admin/entry/');
